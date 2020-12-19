3869063aa189dec14b94e90ee9cdc0ce55da4e8a
Tria Thao <triathao@gmail.com>
2020-12-19 15:48:57 -0800

    testing html

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")))
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
