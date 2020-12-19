d279ec08c992b09fb480c3660a9e7cf88f5904b7
Tria Thao <triathao@gmail.com>
2020-12-19 14:52:14 -0800

    creating routes

--- server.js
+++ server.js
@@ -2,3 +2,9 @@
 const fs = require('fs')
 const http = require('http')
 const express = require('express')
+const path = require("path")
+
+// Express setup App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
